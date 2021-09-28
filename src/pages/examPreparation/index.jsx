dad48a7e565e043c8949623fb04f92d6deab6860
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-09-28 09:17:19 +0700

    Added "exam preparation" page folder (WIP)

---
+++ src/pages/examPreparation/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { DiamondLogo, FreeLogo } from "../../assets";
+import "./examPreparation.scss";
+import { Button } from "../../components/atoms";
+
+const ExamPrep = () => {
+  return <div className="exam__prep">Exam Preparation</div>;
+};
+
+export default ExamPrep;
