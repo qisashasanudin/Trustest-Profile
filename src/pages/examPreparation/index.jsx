4b95c2727f6f60a239c5d509b0c652198071b570
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-10-05 10:05:07 +0700

    added first page of "Exam Preparation"

--- src/pages/examPreparation/index.jsx
+++ src/pages/examPreparation/index.jsx
@@ -2,13 +2,66 @@ import React from "react";
 
 // import { DiamondLogo, FreeLogo } from "../../assets";
 import "./examPreparation.scss";
+
+import { List, ListItem, ListItemText, ListItemAvatar } from "@mui/material";
+import {
+  Wifi as WifiIcon,
+  WbIncandescent as LampIcon,
+  NotificationsOff as MuteIcon,
+} from "@mui/icons-material";
+
 import Button from "@mui/material/Button";
+
+let PageTitle = "Exam Preparation";
+let SubjectName = "Pemrosesan Sinyal Multimedia-01 (2021)";
 
 const ExamPrep = () => {
   return (
     <div className="examprep">
-      <h1>Exam Preparation</h1>
-      <Button variant="contained">Continue</Button>
+      <div className="examprep__header">
+        <h1>{PageTitle}</h1>
+        <p>{SubjectName}</p>
+      </div>
+
+      <div className="examprep__listitems">
+        <List sx={{ width: "100%" }}>
+          <ListItem>
+            <ListItemAvatar>
+              <WifiIcon color="black" fontSize="large" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Stable Internet Connection"
+              secondary="Please ensure you have a stable internet connection."
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <LampIcon color="black" fontSize="large" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Good Lighting"
+              secondary="Please ensure your room has good lighting."
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <MuteIcon color="black" fontSize="large" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Keep Silent and Don’t be Interrupted"
+              secondary="Please keep silent and make sure you are not interrupted during the test, as the timer cannot be paused once started."
+            />
+          </ListItem>
+        </List>
+      </div>
+
+      <div className="examprep__nextbutton">
+        <Button variant="contained" size="large">
+          Continue
+        </Button>
+      </div>
     </div>
   );
 };
