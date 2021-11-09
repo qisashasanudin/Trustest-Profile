6d08a5a5654b67cb4158b91de0d94ae86f67e4f5
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-11-10 01:20:34 +0700

    minor fix

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -7,12 +7,12 @@ import Tests from "../../components/organism/Dashboard/Tests";
 import MyResults from "../../components/organism/Dashboard/MyResults";
 
 import PostList from "../../components/organism/Dashboard/Posts/PostList";
-import PostCreate from "../../components/organism/Dashboard/PostCreate";
-import PostEdit from "../../components/organism/Dashboard/PostEdit";
+import PostCreate from "../../components/organism/Dashboard/Posts/PostCreate";
+import PostEdit from "../../components/organism/Dashboard/Posts/PostEdit";
 
 import UserList from "../../components/organism/Dashboard/Users/UserList";
-import UserCreate from "../../components/organism/Dashboard/UserCreate";
-import UserEdit from "../../components/organism/Dashboard/UserEdit";
+import UserCreate from "../../components/organism/Dashboard/Users/UserCreate";
+import UserEdit from "../../components/organism/Dashboard/Users/UserEdit";
 
 import "./dashboard.scss";
 
